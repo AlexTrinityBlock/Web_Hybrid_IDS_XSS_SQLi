e06308aae7f5ee1a59615471de73c5c50a70e404
AlexTrinityBlock <q123717111@gmail.com>
2023-08-04 00:06:00 +0800

    step

--- codes/ids-frontend/src/lib/gpt-text-box.ts
+++ codes/ids-frontend/src/lib/gpt-text-box.ts
@@ -10,8 +10,17 @@ export const loadLogsAnalysiscache = async function () {
 
 export const loadLogsAnalysis = async function () {
     const idsChatAnalysisObj: HTMLElement | null = document.getElementById("ids-chat-analysis-text")
-    if (idsChatAnalysisObj) {
-        let result: any = await getGPTLogAnalysis()
-        idsChatAnalysisObj.innerText = result.result
-    }
+    const spinnerObj: HTMLElement | null = document.querySelector('#ids-chat-spinner')
+    const btnObj: HTMLElement | null = document.querySelector('#ids-chat-btn')
+    if (!idsChatAnalysisObj) return
+    if(!spinnerObj) return
+    if(!btnObj) return
+    btnObj.style.display = 'none'
+    spinnerObj.style.display= 'inline-block'
+    let result: any = await getGPTLogAnalysis()    
+    idsChatAnalysisObj.innerText = result.result
+    btnObj.style.display = 'inline-block'
+    spinnerObj.style.display= 'none'
+
+
 }
