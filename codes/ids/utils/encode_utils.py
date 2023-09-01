68f94dfbf1e3a7d311fbb622bd5225086876c744
AlexTrinityBlock <q123717111@gmail.com>
2023-09-01 14:19:24 +0800

    Encoder added

--- codes/ids/utils/encode_utils.py
+++ codes/ids/utils/encode_utils.py
@@ -45,19 +45,24 @@ def encode_payload_decode(text: str):
     old_result: str = text
     new_result: str = ''
     while True:
+        # URL encoder
         new_result = urllib.parse.unquote_to_bytes(old_result).decode('utf-8')
+        
+        # HTML entities encoder
         new_result = html.unescape(new_result)
 
+        # Hex encoder
         try:
             new_result = bytes.fromhex(new_result).decode('utf-8')
         except ValueError:
             pass
-        
+
+        # Base64 encoder
         try:
             new_result = base64.b64decode(new_result)
         except binascii.Error:
             pass
-        except  ValueError:
+        except ValueError:
             pass
 
         print('\n\n\n =====Encode Result=====')
